8c5030bf1713eeb0af5e03d01e19fcbf090097d1
Сергей Афонин <belkin23@gmail.com>
2021-12-08 19:08:42 +0700

    Домашнее задание по теме 6. Шаблон проекта

---
+++ Theme6_HomeWork/Theme6_Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//справочник «Сотрудники»
+namespace Theme6_Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
